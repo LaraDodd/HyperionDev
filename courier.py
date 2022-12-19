6b10c8eba1d0a69da73211c96cec073593d08b12
Lara Dodd <laradodd97@gmail.com>
2022-12-19 23:56:57 +0000

    if statements 2

---
+++ courier.py
@@ -0,0 +1,48 @@
+'''This code calculates the cost of sending a parcel'''
+
+distance = int(input("Please can you enter the total distance of the delivery in kms? "))
+
+#  ask user if they would like air or freight?
+air_freight = input("Would you like air or freight delivery method? ").lower()
+
+#  ask user if they would like full or half insurance?
+insurance_type = input("Would you like full insurance of half insurance? ").lower()
+
+#  ask user if they would like it gift wrapped
+gift_wrap = input("Would you like it gift wrapped? yes or no? ").lower()
+
+#  ask user if they would like priority or standard delivery
+priority_standard = input("Would you like priority or standard delivery? ").lower()
+
+if air_freight[0] == "f":  # if the first letter of air_freight string is f (due to user inputting freight)
+    cost_per_km = 0.25
+elif air_freight[0] == "a":
+    cost_per_km = 0.36
+else:
+    print("error, run code again")
+
+if insurance_type[0] == "f":
+    insurance_cost = 50
+elif insurance_type[0] == "h":
+    insurance_cost = 25
+else:
+    print("error, run code again")
+
+if gift_wrap[0] == "y":
+    gift_cost = 15
+elif gift_wrap[0] == "n":
+    gift_cost = 15
+else:
+    print("error, run code again")
+
+if priority_standard[0] == "p":
+    shipping_cost = 100
+elif priority_standard[0] == "s":
+    shipping_cost = 20
+else:
+    print("error, run code again")
+
+#  sum them up
+total_cost = distance*cost_per_km + insurance_cost + gift_cost + shipping_cost
+
+print(f"Calc is {distance}*{cost_per_km} + {insurance_cost} + {gift_cost} + {shipping_cost} = R{total_cost}")
