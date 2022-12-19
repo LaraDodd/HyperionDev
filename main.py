12c745aaae6bcada96c0b48167008b8832e43d95
Lara Dodd <laradodd97@gmail.com>
2022-12-19 13:36:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")  # asks for use to input name and saves as variable 'name'
+print(name)  # prints name
+
+age = input("What is your age? ")  # asks for use to input age and saves as variable 'age'
+print(age)  # prints age
+
+print("Hello World!")
---
+++ menu.py
@@ -0,0 +1,9 @@
+print("Please order your 3 favourite foods from the menu")
+item1 = input("Food item 1 ")
+item2 = input("Food item 2 ")
+item3 = input("Food item 3 ")
+
+print("Order confirmation! You have ordered: ")
+print(item1)
+print(item2)
+print(item3)
