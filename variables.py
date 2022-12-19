1c59525fd64465c0e21a0e4f40a4be5adca7be00
Lara Dodd <laradodd97@gmail.com>
2022-12-19 13:49:59 +0000

    data types

---
+++ variables.py
@@ -0,0 +1,13 @@
+
+#  assign the variables for each type of data type
+float = 4.4
+integer = 4
+string = "four"
+boolean = 4 > 0
+
+#  print out each data type
+print(f"{float} is a {type(float)}")
+print(f"{integer} is a {type(integer)}")
+print(f"{string} is a {type(string)}")
+print(f"{boolean} is a {type(boolean)}")
+
