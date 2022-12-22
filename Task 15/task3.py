d1aad7e35086ee010245dc277862f90f2a3226ea
Lara Dodd <laradodd97@gmail.com>
2022-12-22 15:31:03 +0000

    final commit

--- Task 15/task3.py
+++ Task 15/task3.py
@@ -2,16 +2,14 @@
 # initialise number
 number = 20
 
-#print number whilst it is greater than or equal to 0, reduce number each iteration
+# print number whilst it is greater than or equal to 0, reduce number each iteration
 while number >= 0:
     print(number)
     number -= 1
 
-
 """loop which displays even numbers between 1 and 20"""
-for number in range(2,20,2):
+for number in range(2, 20, 2):
     print(number)
-
 
 """increasing * print"""
 string = ""
@@ -30,17 +28,17 @@ factors1 = []
 user_input_1 = 1238
 user_input_2 = 83
 
-#iterate through all integers between and including 1 and entered number, check if integer is a factor
-for i in range(1, user_input_1+1):
+# iterate through all integers between and including 1 and entered number, check if integer is a factor
+for i in range(1, user_input_1 + 1):
     if user_input_1 % i == 0:
         factors1.append(i)
 
-#repeat for second input
-for i in range(1, user_input_2+1):
+# repeat for second input
+for i in range(1, user_input_2 + 1):
     if user_input_2 % i == 0:
         factors2.append(i)
 
-#while gdc not found, find max of one list and check if that number is in other, if it isn't, remove it from list
+# while gdc not found, find max of one list and check if that number is in other, if it isn't, remove it from list
 while not gcd_found:
     max_factor1 = max(factors1)
     if max_factor1 not in factors2:
@@ -48,4 +46,3 @@ while not gcd_found:
     else:
         print(f"GCD = {max_factor1}")
         gcd_found = True
-
