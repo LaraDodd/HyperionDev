7a39e3ccee72373114b582b2e2bad536954c928f
Lara Dodd <laradodd97@gmail.com>
2022-12-23 10:04:46 +0000

    adjustments to these codes

--- manipulation.py
+++ manipulation.py
@@ -11,8 +11,13 @@ print(last_letter)
 new_string = str_manip.replace(last_letter, "@")  # replacing all instances of the final char with '@'
 print(new_string)
 
-last_3_backwards = str_manip[-3:][::-1]  # get last 3 chars of sentence and then reverse them
+last_3_backwards = str_manip[-1:-4:-1] # get last 3 chars of sentence and then reverse them
 print(last_3_backwards)
 
 five_letter_word = str_manip[:3]+str_manip[-2:]  # concatenating first 3 and last 2 chars in sentence
 print(five_letter_word)
+
+split_sentence = str_manip.split(" ")
+for word in split_sentence:
+    print(word)
+
--- strings.py
+++ strings.py
@@ -1,8 +1,5 @@
 hero = "$$$Superman$$$"
 
-hero.strip('$')
-print(hero)
-
 new_hero = hero.strip('$')
 print(new_hero)
 
