1409cba6724b2219bb377d41f7cb512fc8df572b
Lara Dodd <laradodd97@gmail.com>
2022-12-27 14:12:37 +0000

    initial commit

---
+++ Task 18/list_types.py
@@ -0,0 +1,13 @@
+friend_names = ["Harry Newmann", "Lara Dodd", "Jake Bugg"]
+
+#first friend
+print(f"first friend is {friend_names[0]}")
+
+#last friend
+print(f"last friend is {friend_names[-1]}")
+
+#length of list
+print(f"length of friend list is {len(friend_names)}")
+
+#list of friend ages
+friend_ages = [11, 25, 28]
