2038311086293017c22353e3d396a5f6d90f7fb1
Lara Dodd <laradodd97@gmail.com>
2022-12-27 10:50:24 +0000

    initial commit

--- Task 16/errors.py
+++ Task 16/errors.py
@@ -3,18 +3,18 @@
 # There are some errors in this program, try run the program by pressing F5.
 # Now look at the error messages and find and fix the errors.
 
-print("Welcome to the error program")  # added paranethesis to all print functions
+print("Welcome to the error program")  # added paranethesis to all print functions, syntax error
 print("\n")
 
 ageStr = "24 years old"  # I'm 24 years old.
-age = int(ageStr[:2])  # sliced string to just get 24, then turned into an integer
-print("I'm " + str(age) + " years old.")  # turned age to string, so it could be concatenated with the strings
+age = int(ageStr[:2])  # sliced string to just get 24, then turned into an integer, logical and syntax error
+print("I'm " + str(age) + " years old.")  # turned age to string, so it could be concatenated with the strings, syntax
 three = "3"
 
-answerYears = age + int(three)  # added integer to this
+answerYears = age + int(three)  # added integer to this, syntax
 
-print(f"The total number of years: {answerYears}")  # turned this print to f-string
-answerMonths = answerYears * 12  # changed answer to answerYears
-print("In 3 years and 6 months, I'll be " + str(answerMonths + 6) + " months old")  # added 6 to the total months
+print(f"The total number of years: {answerYears}")  # turned this print to f-string, runtime
+answerMonths = answerYears * 12  # changed answer to answerYears, syntax
+print("In 3 years and 6 months, I'll be " + str(answerMonths + 6) + " months old")  # added 6 to the total months, logical
 
-# HINT, 330 months is the correct answer
+
