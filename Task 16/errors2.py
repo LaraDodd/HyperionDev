3412759aec657fdd7659588407ba48d9f2c5e6e4
Lara Dodd <laradodd97@gmail.com>
2022-12-27 11:14:02 +0000

    initial commit

--- Task 16/errors2.py
+++ Task 16/errors2.py
@@ -3,11 +3,13 @@
 # There are some errors in this program, try run the program by pressing F5.
 # Now look at the error messages and find and fix the errors.
 
-animal = Lion
+animal = "Lion"  # syntax error, changed to string
 animal_type = "cub"
 number_of_teeth = 16
 
-full_spec = "This is a {animal}. It is a {number_of_teeth} and it has {animal_type} teeth"
+# syntax, changed to f-string and logical, switched animal type and num teeth around
+full_spec = f"This is a {animal}. It is a {animal_type} and it has {number_of_teeth} teeth"
 
-print
-full_spec years and 6 months, I'll be " + answerMonths + " months old"
+print(full_spec)  # syntax, added parenthesis
+
+
---
+++ Task 16/logic.py
@@ -0,0 +1,8 @@
+"""this program contains a logical error"""
+
+user_input = int(input("Input a number and the program will tell you whether it is bigger than 10 "))
+
+if user_input > 10:
+    print("it's not bigger than 10")
+else:
+    print("it is bigger than 10")
