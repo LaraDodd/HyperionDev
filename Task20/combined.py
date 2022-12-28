4a1f2cd015ee02a1d47fed6ad3ceefc84e155989
Lara Dodd <laradodd97@gmail.com>
2022-12-28 10:57:47 +0000

    initial commit

--- Task20/combined.py
+++ Task20/combined.py
@@ -1,12 +1,16 @@
-import random
 
 integers = []
 
-for i in range(10):
-    integers.append(random.randint(0,51))
+with open("numbers1.txt", "r+") as f1:
+    for line in f1:
+        integers.append(int(line))
+
+with open("numbers2.txt", "r+") as f2:
+    for line in f2:
+        integers.append(int(line))
 
 integers.sort()
 
-for i in integers:
-    print(i)
-
+with open("all_numbers.txt", "w+") as f3:
+    for integer in integers:
+        f3.write(f"{integer} \n")
