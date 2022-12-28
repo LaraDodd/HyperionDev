bdc8761ed5f3e8abfb439eca1d0d2619e0f3f479
Lara Dodd <laradodd97@gmail.com>
2022-12-28 10:33:51 +0000

    final commit

--- Task20/student_register.py
+++ Task20/student_register.py
@@ -1,10 +1,12 @@
-no_students = 20
+no_students = int(input("How many students are in the class? "))
 student_id_list = []
 
+# cycle through loop and add every student id to a list
 for student in range(no_students):
     student_id = input("Please enter student ID ")
+    student_id_list.append(student_id)
 
+# open a new document called reg form and add each student to it
 with open("reg_form.text", "w+") as f:
     for stud_id in student_id_list:
-        f.write(stud_id+"\n")
-
+        f.write("Student ID: " + str(stud_id) + "\n")
