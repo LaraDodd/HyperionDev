2dcd2886b5cc165e21249dc75e9470199dc07062
Lara Dodd <laradodd97@gmail.com>
2022-12-29 15:34:04 +0000

    username login and adding data to external users.txt

--- Task 21/task_manager.py
+++ Task 21/task_manager.py
@@ -1,7 +1,6 @@
 # =====importing libraries===========
 '''This is the section where you will import libraries'''
 
-
 with open("tasks.txt", "r") as f:
     data = f.readlines()
 
@@ -17,8 +16,6 @@ for pos, line in enumerate(data, 1):
     output += f"-------------------------\n"
     print(output)
 
-
-
 # ====Login Section====
 '''Here you will write code that will allow a user to login.
     - Your code must read usernames and password from the user.txt file
@@ -26,9 +23,50 @@ for pos, line in enumerate(data, 1):
     - Use a while loop to validate your user name and password.
 '''
 
+# read all data in user.txt
+with open("user.txt", "r") as f:
+    user_data = f.readlines()
+
+# initialise lists to append to
+usernames = []
+passwords = []
+
+# go through each line in user_data and split the line into a list of username and password, add to respective lists
+for line in user_data:
+    split_user_data = line.split(", ")
+    usernames.append(split_user_data[0])
+    passwords.append(split_user_data[1].strip("\n"))
+
+# # put all information into a dictionary
+# user_dictionary = {
+#     "Usernames": usernames,
+#     "Passwords": passwords,
+# }
+credentials_not_entered = True
+
+#ask user to enter username and password
+while credentials_not_entered:
+    inputted_username = input("Please enter your username: ")
+    inputted_password = input("Please enter your password: ")
+
+    if inputted_password not in passwords and inputted_username not in usernames:
+        print("These are both not in our system ")
+
+    for i in range(len(usernames)):
+
+        if inputted_username == usernames[i] and not inputted_password == passwords[i]:
+            print("Your password is incorrect try again")
+
+        if inputted_password == passwords[i] and not inputted_username == usernames[i]:
+            print("Your username is incorrect try again")
+
+        if inputted_username == usernames[i] and inputted_password == passwords[i]:
+            print("please wait while we log you in!!")
+            credentials_not_entered = False
+
 while True:
     # presenting the menu to the user and
-    # making sure that the user input is coneverted to lower case.
+    # making sure that the user input is converted to lower case.
     menu = input('''Select one of the following Options below:
 r - Registering a user
 a - Adding a task
@@ -38,9 +76,24 @@ e - Exit
 : ''').lower()
 
     if menu == 'r':
-        pass
-        '''In this block you will write code to add a new user to the user.txt file
-        - You can follow the following steps:
+        '''add a new user to the user.txt file'''
+
+        passwords_match = False
+
+        while not passwords_match:
+            inputted_new_username = input("Please enter a new username: ")
+            inputted_new_password = input("Please enter a new password: ")
+            check_password = input("Please confirm your password: ")
+
+            if check_password == inputted_new_password:
+                with open("user.txt", "a") as f:
+                    f.write(f"\n {inputted_new_username}, {inputted_new_password}")
+                passwords_match = True
+
+            else:
+                print("Sorry the password you entered does not match the first, please try again")
+
+        '''- You can follow the following steps:
             - Request input of a new username
             - Request input of a new password
             - Request input of password confirmation.
@@ -48,43 +101,43 @@ e - Exit
             - If they are the same, add them to the user.txt file,
             - Otherwise you present a relevant message.'''
 
-    elif menu == 'a':
-        pass
-        '''In this block you will put code that will allow a user to add a new task to task.txt file
-        - You can follow these steps:
-            - Prompt a user for the following: 
-                - A username of the person whom the task is assigned to,
-                - A title of a task,
-                - A description of the task and 
-                - the due date of the task.
-            - Then get the current date.
-            - Add the data to the file task.txt and
-            - You must remember to include the 'No' to indicate if the task is complete.'''
-
-    elif menu == 'va':
-        pass
-        '''In this block you will put code so that the program will read the task from task.txt file and
-         print to the console in the format of Output 2 in the task PDF(i.e. include spacing and labelling)
-         You can do it in this way:
-            - Read a line from the file.
-            - Split that line where there is comma and space.
-            - Then print the results in the format shown in the Output 2 
-            - It is much easier to read a file using a for loop.'''
-
-    elif menu == 'vm':
-        pass
-        '''In this block you will put code the that will read the task from task.txt file and
-         print to the console in the format of Output 2 in the task PDF(i.e. include spacing and labelling)
-         You can do it in this way:
-            - Read a line from the file
-            - Split the line where there is comma and space.
-            - Check if the username of the person logged in is the same as the username you have
-            read from the file.
-            - If they are the same print it in the format of Output 2 in the task PDF'''
-
-    elif menu == 'e':
-        print('Goodbye!!!')
-        exit()
-
-    else:
-        print("You have made a wrong choice, Please Try again")
+#     elif menu == 'a':
+#         pass
+#         '''In this block you will put code that will allow a user to add a new task to task.txt file
+#         - You can follow these steps:
+#             - Prompt a user for the following:
+#                 - A username of the person whom the task is assigned to,
+#                 - A title of a task,
+#                 - A description of the task and
+#                 - the due date of the task.
+#             - Then get the current date.
+#             - Add the data to the file task.txt and
+#             - You must remember to include the 'No' to indicate if the task is complete.'''
+#
+#     elif menu == 'va':
+#         pass
+#         '''In this block you will put code so that the program will read the task from task.txt file and
+#          print to the console in the format of Output 2 in the task PDF(i.e. include spacing and labelling)
+#          You can do it in this way:
+#             - Read a line from the file.
+#             - Split that line where there is comma and space.
+#             - Then print the results in the format shown in the Output 2
+#             - It is much easier to read a file using a for loop.'''
+#
+#     elif menu == 'vm':
+#         pass
+#         '''In this block you will put code the that will read the task from task.txt file and
+#          print to the console in the format of Output 2 in the task PDF(i.e. include spacing and labelling)
+#          You can do it in this way:
+#             - Read a line from the file
+#             - Split the line where there is comma and space.
+#             - Check if the username of the person logged in is the same as the username you have
+#             read from the file.
+#             - If they are the same print it in the format of Output 2 in the task PDF'''
+#
+#     elif menu == 'e':
+#         print('Goodbye!!!')
+#         exit()
+#
+#     else:
+#         print("You have made a wrong choice, Please Try again")
