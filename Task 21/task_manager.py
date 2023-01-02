42aa7fc155596279c05db4a4d558812f704324cb
Lara Dodd <laradodd97@gmail.com>
2023-01-02 16:29:42 +0000

    putting GUI into task manager

--- Task 21/task_manager.py
+++ Task 21/task_manager.py
@@ -1,7 +1,32 @@
 # =====importing libraries===========
 from datetime import date
+from tkinter import *
+from tkinter import messagebox
 
 # ====Login Section====
+
+def login():
+    inputted_username = username.get()
+    inputted_password = password.get()
+
+    if inputted_password not in passwords and inputted_username not in usernames:
+        messagebox.showinfo("Incorrect Credentials", "These are both not in our system")
+        return True
+
+    for i in range(len(usernames)):
+        if inputted_username == usernames[i] and not inputted_password == passwords[i]:
+            messagebox.showinfo("Incorrect Credentials", "Your password is incorrect try again")
+            return True
+
+        if inputted_password == passwords[i] and not inputted_username == usernames[i]:
+            messagebox.showinfo("Incorrect Credentials", "Your username is incorrect try again")
+            return True
+
+        if inputted_username == usernames[i] and inputted_password == passwords[i]:
+            messagebox.showinfo("Login Successful", "Please wait while we log you in!!")
+            tkWindow.destroy()
+            return False
+
 
 # read all data in user.txt
 with open("user.txt", "r") as f:
@@ -10,7 +35,6 @@ with open("user.txt", "r") as f:
 # initialise lists to append to and initialise while loop bool
 usernames = []
 passwords = []
-credentials_not_entered = True
 
 # go through each line in user_data and split the line into a list of username and password, add to respective lists
 for line in user_data:
@@ -18,25 +42,29 @@ for line in user_data:
     usernames.append(split_user_data[0])
     passwords.append(split_user_data[1].strip("\n"))
 
+# set up tk window
+tkWindow = Tk()
+tkWindow.geometry('250x100')
+tkWindow.title('Task Manager Login')
+
 # ask user to enter username and password
-while credentials_not_entered:
-    inputted_username = input("Please enter your username: ")
-    inputted_password = input("Please enter your password: ")
+# while True:
 
-    if inputted_password not in passwords and inputted_username not in usernames:
-        print("These are both not in our system ")
+# username label and text entry box
+Label(tkWindow, text="User Name").grid(row=0, column=500)
+username = StringVar()
+Entry(tkWindow, textvariable=username).grid(row=0, column=600)
 
-    for i in range(len(usernames)):
+# password label and password entry box
+Label(tkWindow, text="Password").grid(row=1, column=500)
+password = StringVar()  # holder for string variables
+Entry(tkWindow, textvariable=password).grid(row=1, column=600)
 
-        if inputted_username == usernames[i] and not inputted_password == passwords[i]:
-            print("Your password is incorrect try again")
+# login button
+Button(tkWindow, text="Login", command=login).grid(row=4, column=600)
 
-        if inputted_password == passwords[i] and not inputted_username == usernames[i]:
-            print("Your username is incorrect try again")
+tkWindow.mainloop()
 
-        if inputted_username == usernames[i] and inputted_password == passwords[i]:
-            print("please wait while we log you in!!")
-            credentials_not_entered = False
 
 # ========choose option section ==========
 
--- Task 21/test.py
+++ Task 21/test.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from functools import partial
 from tkinter import messagebox
 
 
@@ -9,15 +8,21 @@ def login():
 
     if inputted_password not in passwords and inputted_username not in usernames:
         messagebox.showinfo("Incorrect Credentials", "These are both not in our system")
+        return True
 
     for i in range(len(usernames)):
         if inputted_username == usernames[i] and not inputted_password == passwords[i]:
             messagebox.showinfo("Incorrect Credentials", "Your password is incorrect try again")
+            return True
+
         if inputted_password == passwords[i] and not inputted_username == usernames[i]:
             messagebox.showinfo("Incorrect Credentials", "Your username is incorrect try again")
+            return True
+
         if inputted_username == usernames[i] and inputted_password == passwords[i]:
             messagebox.showinfo("Login Successful", "Please wait while we log you in!!")
-            return credentials_not_entered == False
+            tkWindow.destroy()
+            return False
 
 
 # read all data in user.txt
@@ -27,7 +32,6 @@ with open("user.txt", "r") as f:
 # initialise lists to append to and initialise while loop bool
 usernames = []
 passwords = []
-credentials_not_entered = True
 
 # go through each line in user_data and split the line into a list of username and password, add to respective lists
 for line in user_data:
@@ -35,25 +39,28 @@ for line in user_data:
     usernames.append(split_user_data[0])
     passwords.append(split_user_data[1].strip("\n"))
 
+# set up tk window
 tkWindow = Tk()
 tkWindow.geometry('250x100')
 tkWindow.title('Task Manager Login')
 
 # ask user to enter username and password
-while credentials_not_entered:
-    # username label and text entry box
-    Label(tkWindow, text="User Name").grid(row=0, column=500)
-    username = StringVar()
-    Entry(tkWindow, textvariable=username).grid(row=0, column=600)
+# while True:
 
-    # password label and password entry box
-    Label(tkWindow, text="Password").grid(row=1, column=500)
-    password = StringVar()
-    Entry(tkWindow, textvariable=password).grid(row=1, column=600)
+# username label and text entry box
+Label(tkWindow, text="User Name").grid(row=0, column=500)
+username = StringVar()
+Entry(tkWindow, textvariable=username).grid(row=0, column=600)
 
-    # login button
-    Button(tkWindow, text="Login", command=login).grid(row=4, column=600)
+# password label and password entry box
+Label(tkWindow, text="Password").grid(row=1, column=500)
+password = StringVar()  # holder for string variables
+Entry(tkWindow, textvariable=password).grid(row=1, column=600)
 
-    tkWindow.mainloop()
+# login button
+Button(tkWindow, text="Login", command=login).grid(row=4, column=600)
 
-tkWindow.quit()
+tkWindow.mainloop()
+
+
+
