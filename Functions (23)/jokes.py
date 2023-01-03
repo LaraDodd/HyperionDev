a523e8d57ed199c897223a4a79b731b7413506fe
Lara Dodd <laradodd97@gmail.com>
2023-01-03 14:33:53 +0000

    generating random jokes initial commit

---
+++ Functions (23)/jokes.py
@@ -0,0 +1,24 @@
+import pyjokes
+import random
+
+"""generate list of jokes in text file"""
+# using get_jokes() to generate a whole list of jokes
+list_of_jokes = pyjokes.get_jokes(language="en", category="all")
+
+#write jokes to separate txt doc
+with open("jokes.txt", "w") as jokes:
+    for joke in list_of_jokes:
+        jokes.write(f"{joke}\n")
+
+"""main code of creating list of jokes"""
+#create list of jokes
+jokes_list
+with open("jokes.txt", "r") as jokes:
+    for
+
+def generate_rand_joke():
+
+
+
+
+
